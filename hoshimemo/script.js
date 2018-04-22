fb6cb53e279a7b5622cf2d4ec61161e102958fbe
xubeiyan <yueyongdong@gmail.com>
2018-04-22 23:33:41 +0800

    add many files...

--- hoshimemo/script.js
+++ hoshimemo/script.js
@@ -1,12 +1,16 @@
 var img = [],
 	loaded = 0,
+	canvasStage = document.getElementById("stage"),
 	preload = function (imgLists) {
+		canvasStage.width = window.innerWidth;
+		canvasStage.height = canvasStage.width * 3 / 4;
+		
 		for (var i = 0; i < imgLists.length; ++i) {
 			img[i] = new Image();
 			img[i].src = imgLists[i];
 			img[i].onload = function () {
 				loaded += 1;
-				if (loaded == 4) {
+				if (loaded == 3) {
 					brickDraw(0, 0, img[0]);
 				}
 			}
@@ -23,26 +27,36 @@ var img = [],
 			currentImg = next + 1;
 		}
 	},
-	canvasStage = document.getElementById("stage"),
+	
 	canvasContext = canvasStage.getContext("2d"),
-	grid = 64,
-	widthLength = canvasStage.width / grid,
-	heightLength = canvasStage.height / grid,
+	widthGrid = 16,
+	heightGrid = 12,
+	widthLength = 1600 / widthGrid,
+	heightLength = 1200 / heightGrid,
 	brickDrawGridTime = 100,
 	brickDrawPicTime = 2000,
 	brickDraw = function (x, y, image) {
 		
-		// canvasContext.drawImage(img[currentImg], 0, 0);
+		var sW = window.innerWidth,
+			sH = window.innerWidth * image.height / image.width,
+			dW = image.width,
+			dH = image.height;
+		// console.log('height: ', image.height)
+		// console.log('width: ', image.width)
+		// console.log('sw:', sW, 'sH:', sH);
+		// console.log('dw:', dW, 'dH:', dH);
+		
+		// canvasContext.drawImage(image, 0, 0, sW, sH);
 		console.log(x, y);
-		canvasContext.drawImage(image, x * grid, y * grid,
-			grid, grid, x * grid, y * grid, grid, grid);
+		canvasContext.drawImage(image, x * widthLength, y * heightLength,
+			widthLength, widthLength, x * widthLength, y * heightLength, widthLength, widthLength);
 			
-		if (x + 1 < widthLength) {
+		if (x + 1 < widthGrid) {
 			setTimeout(function () {
 				brickDraw(x + 1, y, image);
 			}, brickDrawGridTime);		
 		} else {
-			if (y + 1 < heightLength) {
+			if (y + 1 < heightGrid) {
 				x = 0;
 				setTimeout(function () {
 					brickDraw(x, y + 1, image);
@@ -54,16 +68,25 @@ var img = [],
 				}, brickDrawPicTime);
 			}
 		}
-	}
+	},
+	resize = function () {
+		// console.log(window.innerWidth, window.innerHeight);
+		canvasStage.width = window.innerWidth;
+		canvasStage.height = canvasStage.width * 3 / 4;
+		// canvasStage.style.width = '' + canvasStage.width + 'px';
+		// canvasStage.style.height = '' + canvasStage.height + 'px';
+		brickDraw(0, 0, img[0]);
+	};
 	
 window.onload = function () {
 	preload([
-		'hoshimemo/01.jpg',
-		'hoshimemo/02.jpg',
-		'hoshimemo/03.jpg',
-		'hoshimemo/04.jpg',
+		'hoshimemo/03-2.jpg',
+		// 'hoshimemo/05.jpg',
+		'hoshimemo/06.jpg',
+		'hoshimemo/07.jpg',
 	]);
+};
 
-}
+window.addEventListener('resize', resize, false);
 	
 
