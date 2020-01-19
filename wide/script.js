c6711a691e222cee515d45f9ef67c1b5200052f6
xubeiyan <yueyongdong@gmail.com>
2020-01-19 16:28:27 +0800

    add a new wide version for it...

---
+++ wide/script.js
@@ -0,0 +1,73 @@
+/**
+* 时隔多年重新编写此文件
+* 当年注释真的都没写（我真是太菜了）
+* 而且setTimeout用得好烂啊
+*/
+
+var canvas = document.getElementById('stage'),
+	context = canvas.getContext('2d'),
+	// 切换等待用时
+	elapseTime = 2000,
+	img = [],
+	// 绘制状态loading, drawing, error
+	status = 'loading', 
+	// 调整窗口大小后重新计算canvas大小
+	resizeWindow = function () {
+		canvas.style.width = window.innerWidth;
+		canvas.style.height = window.innerWidth * 9 / 16 + 'px';
+		console.log(`[STATUS]to ${status}`);
+	},
+	// 图片的列表
+	imageList = [
+		'wide/MARE_e01n.jpg', 
+		'wide/MARE_e09b.jpg', 
+		'wide/MARE_FD_e03a.jpg', 
+		'wide/MARE_FD_e04b.jpg', 
+		'wide/MARE_FD_e05c.jpg', 
+		'wide/MARE_FD_e07f.jpg'
+	],
+	// 根据加载成功的图片的长度随机一个图片来显示
+	randomImageIndex = function (last) {
+		var len = img.length,
+			current = last;
+		while (last == current) {
+			current = Math.floor(len * Math.random());
+		}
+		return current;
+	},
+	// 加载图片
+	loadImage = function () {
+		for(var i = 0; i < imageList.length; i++) {
+			// var声明会导致其失去变化，只能用let。TODO:看有没有更优雅的方式来实现
+			let newimg = new Image();
+			newimg.src = imageList[i];
+			// 图片加载成功后回调函数
+			newimg.onload = function () {
+				console.log(`[IMAGE]loaded "${newimg.src}" success!`);
+				img.push(newimg);
+				// 如果有图片加载成功了且绘制状态为loading则开始绘制
+				if (img.length > 1 && status == 'loading') {
+					draw(0);
+					status = 'drawing';
+					console.log(`[STATUS]to ${status}`);
+				}
+			}
+		}
+	},
+	// 绘制图片
+	draw = function (index) {
+		context.drawImage(img[index], 0, 0);
+		setTimeout(function () {
+			var next = randomImageIndex(index);
+			draw(next);
+		}, elapseTime)
+	}
+	
+resizeWindow();
+loadImage(imageList);
+
+
+window.addEventListener('resize', function () {
+	resizeWindow();
+}, false);
+
