3172a90e26a9c19835e68dc22a72006d4499408e
xubeiyan <yueyongdong@gmail.com>
2022-02-16 20:51:09 +0800

    modify the clock style... todos: remove the red line of hour hand and minute hand...

--- chenhai.net/tools/clock/js/clock.js
+++ chenhai.net/tools/clock/js/clock.js
@@ -1,28 +1,34 @@
 var canvas = document.getElementById('canvas'),
 	context = canvas.getContext('2d'),
+	CIRCLE_PADDING = 5,
 	FONT_HEIGHT = 20,
-	MARGIN = 35,
-	HAND_TRUNCATION = canvas.width/25,
+	MARGIN = 5,
+	HAND_TRUNCATION = canvas.width/15,
 	HOUR_HAND_TRUNCATION = canvas.width/10,
 	NUMERAL_SPACING = 20,
 	RADIUS = canvas.width/2 - MARGIN,
-	HAND_RADIUS = RADIUS + NUMERAL_SPACING;
+	HAND_RADIUS = RADIUS - NUMERAL_SPACING;
 
 //Function
 // 画出钟圈
 function drawCircle() {
+	// 外层
 	context.beginPath();
 	context.arc(canvas.width / 2, canvas.height / 2, RADIUS, 0, Math.PI * 2, true);
+	context.stroke();
+	// 内层
+	context.beginPath();
+	context.arc(canvas.width / 2, canvas.height / 2 , RADIUS - CIRCLE_PADDING, 0, Math.PI * 2, true);
 	context.stroke();
 }
 
 // 画出数字
 function drawNumerals() {
-	var numerals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-		angle = 0,
-		numeralWidth = 0;
+	const numerals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+	let	angle = 0;
+	let numeralWidth = 0;
 		
-	numerals.forEach(function (numeral){
+	numerals.forEach((numeral) => {
 		angle = Math.PI / 6 * (numeral - 3);
 		numeralWidth = context.measureText(numeral).width;
 		context.fillText(numeral,
@@ -42,14 +48,19 @@ function drawCenter() {
 }
 
 // 画出针
-function drawHand(loc, isHour) {
+function drawHand(loc, isHour, width, color="#000") {
 	var angle = (Math.PI*2) * (loc/60) - Math.PI/2,
 		handRadius = isHour ? RADIUS - HAND_TRUNCATION - HOUR_HAND_TRUNCATION : RADIUS - HAND_TRUNCATION;
 	
+	context.save();
 	context.moveTo(canvas.width/2, canvas.height/2);
+	context.lineWidth = width;
+	context.fillStyle = color;
+	context.strokeStyle = color;
 	context.lineTo(canvas.width/2 + Math.cos(angle) * handRadius, 
 		canvas.height/2 + Math.sin(angle) * handRadius);
 	context.stroke();
+	context.restore();
 }
 
 // 画出三根针
@@ -59,9 +70,9 @@ function drawHands() {
 	
 	hour = hour > 12 ? hour - 12 : hour;
 	
-	drawHand(hour * 5 + (date.getMinutes()/60)*5, true, 0.5);
-	drawHand(date.getMinutes(), false, 0.5);
-	drawHand(date.getSeconds(), false, 0.2);
+	drawHand(date.getMinutes(), false, 6, 'black');
+	drawHand(hour * 5 + (date.getMinutes()/60)*5, true, 8, 'black');
+	drawHand(date.getSeconds(), false, 1, 'red');
 }
 
 // 画出钟表
