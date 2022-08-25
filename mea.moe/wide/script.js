bfa96c6672b7ea9fc1f63c800be8cede8763c0ad
xubeiyan <yueyongdong@gmail.com>
2022-08-25 09:06:26 +0800

    rewrite wide version, add NotoSansJP-Regular.woff2 as font

--- mea.moe/wide/script.js
+++ mea.moe/wide/script.js
@@ -4,79 +4,129 @@
 * 而且setTimeout用得好烂啊
 */
 
-var canvas = document.getElementById('stage'),
-	context = canvas.getContext('2d'),
-	// 输出日志
-	logLevel = 'info', // 有info, warn, none三个级别
-	log = function (type, content) {
-		if (logLevel == 'info') {
-			console.log(`[${type}]${content}`);
-		}
-	},
-	// 切换等待用时
-	elapseTime = 5000,
-	img = [],
-	// 绘制状态loading, drawing, error
-	status = 'loading', 
-	// 调整窗口大小后重新计算canvas大小
-	resizeWindow = function () {
-		canvas.style.width = window.innerWidth;
-		canvas.style.height = window.innerWidth * 9 / 16 + 'px';
-		console.log(`[STATUS]to ${status}`);
-	},
-	// 图片的列表
-	imageList = [
-		'wide/MARE_e01n.jpg', 
-		'wide/MARE_e09b.jpg', 
-		'wide/MARE_FD_e03a.jpg', 
-		'wide/MARE_FD_e04b.jpg', 
-		'wide/MARE_FD_e05c.jpg', 
-		'wide/MARE_FD_e07f.jpg'
-	],
-	// 根据加载成功的图片的长度随机一个图片来显示
-	randomImageIndex = function (last) {
-		var len = img.length,
-			current = last;
-		while (last == current) {
-			current = Math.floor(len * Math.random());
-		}
-		return current;
-	},
-	// 加载图片
-	loadImage = function () {
-		for(var i = 0; i < imageList.length; i++) {
-			// var声明会导致其失去变化，只能用let。TODO:看有没有更优雅的方式来实现
-			let newimg = new Image();
-			newimg.src = imageList[i];
-			// 图片加载成功后回调函数
-			newimg.onload = function () {
-				log('IMAGE', `loaded "${newimg.src}" success!`);
-				img.push(newimg);
-				// 如果有2张图片加载成功了且绘制状态为loading则开始绘制
-				if (img.length > 1 && status == 'loading') {
-					draw(0);
-					status = 'drawing';
-					log('STATUS', `to ${status}`);
-				}
+const canvas = document.getElementById('stage');
+const context = canvas.getContext('2d');
+// 输出日志
+let logLevel = 'info'; // 有info, warn, none三个级别
+let log = function (type, content) {
+	if (logLevel == 'info') {
+		console.log(`[${type}]${content}`);
+	}
+};
+
+// 切换等待用时
+let clipSwitchTime = 20;
+let elapseTime = 5000;
+// 加载
+let loadedImage = [];
+// 绘制状态loading, drawing, error
+let status = 'loading';
+// 调整窗口大小后重新计算canvas大小
+const resizeWindow = () => {
+	canvas.style.width = window.innerWidth;
+	canvas.style.height = window.innerWidth * 9 / 16;
+	console.log(`[STATUS]window resize to ${status}`);
+};
+// 图片的列表
+let imageList = [
+	'wide/MARE_e01n.jpg',
+	'wide/MARE_e09b.jpg',
+	'wide/MARE_FD_e03a.jpg',
+	'wide/MARE_FD_e04b.jpg',
+	'wide/MARE_FD_e05c.jpg',
+	'wide/MARE_FD_e07f.jpg'
+];
+
+// 根据加载成功的图片的长度随机一个图片来显示
+const randomImageIndex = function (last) {
+	const len = loadedImage.length;
+	let	current = last;
+	while (last == current) {
+		current = Math.floor(len * Math.random());
+	}
+	return current;
+}
+
+// 生成从0~length-1的乱序数列
+const getShuffleOrder = (length) => {
+	let arr = new Array(length);
+
+	// fill 
+	for (let i = 0; i < arr.length; ++i) {
+		arr[i] = i;
+	}
+
+	let m = length;
+
+	for (let i, temp; m > 0; --m) {
+		i = Math.floor(Math.random() * m);
+		temp = arr[m - 1];
+		arr[m - 1] = arr[i];
+		arr[i] = temp;
+	}
+
+	return arr;
+}
+
+// 加载图片
+const loadImage = () => {
+	imageList.forEach(one => {
+		let newimg = new Image();
+		newimg.src = one;
+		// 图片加载成功后回调函数
+		newimg.onload = () => {
+			log('IMAGE', `loaded "${newimg.src}" success!`);
+			loadedImage.push(newimg);
+			// 如果有2张图片加载成功了且绘制状态为loading则开始绘制
+			if (loadedImage.length > 1 && status == 'loading') {
+				draw(0);
+				status = 'drawing';
+				log('STATUS', `to ${status}`);
 			}
 		}
-	},
-	// 绘制图片
-	draw = function (index) {
-		context.drawImage(img[index], 0, 0);
-		setTimeout(function () {
-			var next = randomImageIndex(index);
-			draw(next);
-		}, elapseTime)
-	}
+	});
+};
+// 绘制图片
+const draw = (index) => {
+	let clipHoriAmount = 32;
+	let clipVertAmount = 18;
+	let clipWidth = 3840 / clipHoriAmount;
+	let clipHeight = 2160 / clipVertAmount;
+	// 绘制顺序，按照
+	let drawOrder = getShuffleOrder(clipHoriAmount * clipVertAmount);
+
+	let drawClip = (clipIndex, drawOrder) => {
+		if (clipIndex >= drawOrder.length) {
+			// console.log('done');
+			setTimeout(function () {
+				const next = randomImageIndex(index);
+				draw(next);
+			}, elapseTime);
+			return;
+		};
+		// drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+		let actualOrder = drawOrder[clipIndex];
+		let sx = (actualOrder % clipHoriAmount) * clipWidth;
+		let sy = Math.floor(actualOrder / clipHoriAmount) * clipHeight;
+		context.drawImage(loadedImage[index], sx, sy, clipWidth, clipHeight, sx, sy, clipWidth, clipHeight);
+
+		setTimeout(() => {
+			drawClip(clipIndex + 1, drawOrder);
+		}, clipSwitchTime);
+	};
+
+	drawClip(0, drawOrder);
+
 	
+}
+
 // 页面加载完成后开始加载图片
-window.addEventListener('load', function() {
+window.addEventListener('load', () => {
 	loadImage(imageList);
 });
 
 // 缩放窗口触发调整函数
-window.addEventListener('resize', function () {
+window.addEventListener('resize', () => {
 	resizeWindow();
 }, false);
 
